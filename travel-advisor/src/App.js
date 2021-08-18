255e2beef80b3e9387a9df94e88f6f1e9550e3d3
Oleh <oleh.m97@gmail.com>
2021-08-18 23:13:21 +0300

    vxc

--- travel-advisor/src/App.js
+++ travel-advisor/src/App.js
@@ -1,7 +1,16 @@
-const App = () => (
-    <div className="App">
+import React from "react";
+import {CssBaseline, Grid} from "@material-ui/core";
+import Header from "./components/Header/Header";
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Header/>
+            <Grid container spacing={3} style={{width: '100%'}}>
 
-    </div>
-);
+            </Grid>
+        </>
+    )
+}
 
 export default App;
