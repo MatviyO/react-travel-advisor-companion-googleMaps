73c22a84136717447478d26ea58cbbe07412602c
Oleh <oleh.m97@gmail.com>
2021-08-26 23:44:10 +0300

    up

--- travel-advisor/src/components/List/List.jsx
+++ travel-advisor/src/components/List/List.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import Rating from '@material-ui/lab';
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-
 const List = () =>  {
     return (
         <div>
--- travel-advisor/src/components/Map/Map.jsx
+++ travel-advisor/src/components/Map/Map.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import Rating from '@material-ui/lab';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import useStyles from './style';
+import {useMediaQuery} from "@material-ui/core";
 
 const Map = () => {
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(min-width: 600px)')
+    const coordinates = {lat: 0, lng: 0}
     return (
-        <div>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: 'AIzaSyAhEqwDYyJ0jEtIFYo6aeL3jrdlQD8oSM4&callback'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
 
         </div>
     )
