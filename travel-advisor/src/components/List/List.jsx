9be2c8a3c82eca16dc12db0030d60b5f7e9b77ec
Oleh <oleh.m97@gmail.com>
2021-08-26 23:10:22 +0300

    up

--- travel-advisor/src/components/List/List.jsx
+++ travel-advisor/src/components/List/List.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import Rating from '@material-ui/lab';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 
 const List = () =>  {
     return (
