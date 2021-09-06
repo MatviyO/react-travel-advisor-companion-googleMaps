c78f9bc97466dbf89aa8c0bce2e95ecb67096641
Oleh <oleh.m97@gmail.com>
2021-09-06 23:39:32 +0300

    up

--- travel-advisor/src/components/PlaceDetails/PlaceDetails.tsx
+++ travel-advisor/src/components/PlaceDetails/PlaceDetails.tsx
@@ -2,12 +2,15 @@ import React, {FC} from 'react';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
+import useStyles from './style';
 
 interface Props {
     place: { name: string}
 }
 
+
 const PlaceDetails: FC<Props> = ({place}) =>  {
+    const classes = useStyles()
     return (
         <h1>{place.name}</h1>
     )
