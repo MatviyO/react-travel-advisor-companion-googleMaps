52b69189bab99781ea15353c2a2ceee0d02e21dd
Oleh <oleh.m97@gmail.com>
2021-09-10 22:30:48 +0300

    up

--- travel-advisor/src/components/PlaceDetails/PlaceDetails.tsx
+++ travel-advisor/src/components/PlaceDetails/PlaceDetails.tsx
@@ -3,16 +3,21 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
 import useStyles from './style';
+import {Card, CardMedia} from "@material-ui/core";
 
 interface Props {
-    place: { name: string}
+    place: { name: string, photo: any}
 }
-
 
 const PlaceDetails: FC<Props> = ({place}) =>  {
     const classes = useStyles()
     return (
-        <h1>{place.name}</h1>
+        <Card elevation={6} >
+            <CardMedia
+                title={place.name}
+                style={{height: 350}} image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+            />
+        </Card>
     )
 }
 
